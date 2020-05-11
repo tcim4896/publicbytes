ff97a1d36718404eaef353e4b9b96f7be341f07a
tcim4896 <47268866+tcim4896@users.noreply.github.com>
2020-05-11 12:31:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log(“welcome to publicbytes”)
+
+const express = require('express')
+const app = express()
+const port = 80;
+
+app.get('/', (req, res) => res.send('Welcome to publicbytes!'))
+
+app.listen(port, () => console.log(`Publicbytes listening at http://localhost:${port}`))
